063096ccc95a299a45f879f90c4d9dc2d596730d
sonu2 <rajbhar9969@gmail.com>
2024-11-06 21:06:47 +0530

    ttt

--- Services/DashBoardServices.js
+++ Services/DashBoardServices.js
@@ -4,8 +4,8 @@ const User = require('../Models/DashBoardModel');
 
 exports.getAllUsers = async () => {
   try {
-    const users = await User.find(); 
-     // No callback here, just await the result
+    const users = await User.find();
+    // No callback here, just await the result
     console.log('Users:', users);  // Logs the users
     return users;  // Return the fetched users
   } catch (err) {
@@ -16,12 +16,26 @@ exports.getAllUsers = async () => {
 
 exports.Create = async (data) => {
   try {
-    const response = await User.insertMany(data); 
-     // No callback here, just await the result
-    console.log('User:', response);  // Logs the users
-    return response;  // Return the fetched users
+    if (Data._id) {
+      // Update the existing company document
+      const response = await user.findByIdAndUpdate(
+        Data._id,  // Document ID for update
+        Data,     // Updated data
+        { new: true, upsert: true }  // Options to return the updated doc and insert if not found
+      );
+
+      console.log('Updated Employee:', response);
+      return response;
+
+    } else {
+      const response = await User.insertMany(data);
+      // No callback here, just await the result
+      console.log('User:', response);  // Logs the users
+      return response;  // Return the fetched users
+    }
   } catch (err) {
     console.error('Error fetching users:', err);  // Log and handle any errors
     throw err;  // Rethrow the error for upstream error handling
   }
+
 };
